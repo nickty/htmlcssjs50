6d92ebd9e3e9fd3bb9ddf8c528e6668f20c4ac24
Mizanur Rahman <mizanur.rahman@navana.com>
2022-02-11 08:55:33 +0600

    hidden search widget

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 /** @format */
+const search = document.querySelector('.search');
+const btn = document.querySelector('.btn');
+const input = document.querySelector('.input');
 
-const open = document.getElementById('open');
-const close = document.getElementById('close');
-
-const container = document.querySelector('.container');
-
-open.addEventListener('click', () => container.classList.add('show-nav'));
-close.addEventListener('click', () => container.classList.remove('show-nav'));
+btn.addEventListener('click', () => {
+  search.classList.toggle('active');
+  input.focus();
+});
