09080c81b0d842d91201aaa1f29a8ce8f5627cd4
Mizanur Rahman <mizanur.rahman@navana.com>
2022-02-11 14:51:11 +0600

    splite landing page

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 /** @format */
-const boxes = document.querySelectorAll('.box');
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+const container = document.querySelector('.container');
 
-window.addEventListener('scroll', checkBoxes);
-
-function checkBoxes() {
-  const triggerBottom = (window.innerHeight / 5) * 4;
-
-  boxes.forEach((box) => {
-    const boxTop = box.getBoundingClientRect().top;
-
-    if (boxTop < triggerBottom) {
-      box.classList.add('show');
-    } else {
-      box.classList.remove('show');
-    }
-  });
-}
+left.addEventListener('mouseenter', () =>
+  container.classList.add('hover-left')
+);
+left.addEventListener('mouseleave', () =>
+  container.classList.remove('hover-left')
+);
+right.addEventListener('mouseenter', () =>
+  container.classList.add('hover-right')
+);
+right.addEventListener('mouseleave', () =>
+  container.classList.remove('hover-right')
+);
