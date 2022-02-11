a50729f28d7bab4c9fd9331f67a3f9dc616345bb
Mizanur Rahman <mizanur.rahman@navana.com>
2022-02-11 08:40:38 +0600

    navigation flip

--- script.js
+++ script.js
@@ -1,56 +1,9 @@
 /** @format */
 
-const progress = document.getElementById('progress');
-const prev = document.getElementById('prev');
-const next = document.getElementById('next');
-const circles = document.querySelectorAll('.circle');
+const open = document.getElementById('open');
+const close = document.getElementById('close');
 
-let currentActive = 1;
+const container = document.querySelector('.container');
 
-next.addEventListener('click', () => {
-  currentActive++;
-
-  console.log(currentActive);
-
-  if (currentActive > circles.length) {
-    currentActive = circles.length;
-  }
-
-  update();
-});
-
-prev.addEventListener('click', () => {
-  currentActive--;
-
-  console.log(currentActive);
-
-  if (currentActive < 1) {
-    currentActive = 1;
-  }
-
-  update();
-});
-
-function update() {
-  circles.forEach((circle, index) => {
-    if (index < currentActive) {
-      circle.classList.add('active');
-    } else {
-      circle.classList.remove('active');
-    }
-  });
-
-  const actives = document.querySelectorAll('.active');
-
-  progress.style.width =
-    ((actives.length - 1) / (circles.length - 1)) * 100 + '%';
-
-  if (currentActive === 1) {
-    prev.disabled = true;
-  } else if (currentActive === circles.length) {
-    next.disabled = true;
-  } else {
-    prev.disabled = false;
-    next.disabled = false;
-  }
-}
+open.addEventListener('click', () => container.classList.add('show-nav'));
+close.addEventListener('click', () => container.classList.remove('show-nav'));
